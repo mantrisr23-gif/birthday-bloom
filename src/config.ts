6500bcee46fc3a91396c0aa907eac541d8874ac4
mantrisr23-gif <mantrisr23@gmail.com>
2026-04-29 11:27:02 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,13 @@
  * This file is for beginners who don't want to use Environment Variables.
  */
 export const config = {
-    // Method 2: Fallback Name (Only used if VITE_BIRTHDAY_NAME is not set in .env)
-    birthdayName: "AKASH",
+  // Method 1: Fallback Name
+  birthdayName: "AKASH",
 
-    // Method 2: Fallback Photos (Only used if assets/photoX.jpg are missing)
-   photos: [
-        "/funny1.jpg",
-        "/funny2.jpg",
-        "/funny3.jpg"
-]
-    ]
+  // Method 2: Fallback Photos
+  photos: [
+    "/funny1.jpg",
+    "/funny2.jpg",
+    "/funny3.jpg"
+  ]
 };
