0aa3f2097872ec6abc8621a6925187f398387f14
mantrisr23-gif <mantrisr23@gmail.com>
2026-04-29 09:44:14 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,14 @@
  */
 export const config = {
     // Method 2: Fallback Name (Only used if VITE_BIRTHDAY_NAME is not set in .env)
-    birthdayName: "YOU",
+    birthdayName: "AKASH",
 
     // Method 2: Fallback Photos (Only used if assets/photoX.jpg are missing)
     photos: [
-        "https://images.unsplash.com/photo-1530103043960-ef38714abb15",
-        "https://images.unsplash.com/photo-1464349095431-e9a21285b5f3",
-        "https://images.unsplash.com/photo-1513151233558-d860c5398176"
+       photos: [
+    "funny1.jpg",
+    "funny2.jpg",
+    "funny3.jpg"
+]
     ]
 };
